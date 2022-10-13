06323c4de5b5b9efe964ac00936c646753e1788f
ohsuk kwon <percykwon@gmail.com>
2022-10-13 13:14:48 +0900

    [221013] 서울시 병의원 분석 1. parse 로직 추가 2. parsing test 추가

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -10,25 +10,44 @@ public class Hospital {
     private String name;
     private String subdivision;
 
-    public Hospital(String id, String address, String district, String category, String emergencyRoom, String name, String subdivision) {
+    public Hospital(String id, String address, String district, String category, String emergencyRoom, String name) {
         this.id = id;
         this.address = address;
         this.district = district;
         this.category = category;
         this.emergencyRoom = emergencyRoom;
         this.name = name;
-        this.subdivision = subdivision;
-    }
-
-    public Hospital(String id) {
-        this.id = id.replaceAll("\"","");
     }
 
     public String getId() {
         return id;
     }
 
-//        public List<Hospital> readAndParse(String filename) {
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
+    //        public List<Hospital> readAndParse(String filename) {
 //        ArrayList<Hospital> result = new ArrayList<>();
 //
 //        return result;
--- src/main/java/com/line/parser/HostpitalParser.java
+++ src/main/java/com/line/parser/HostpitalParser.java
@@ -3,9 +3,19 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HostpitalParser implements Parser<Hospital> {
+
+
+    private String replaceAllQuot(String str) {
+        return str.replaceAll("\"", "");
+    }
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+
+        String[] splitted_address = splitted[1].split(" ");
+        String district = splitted_address[0] + " " + splitted_address[1];
+
+        return new Hospital(replaceAllQuot(splitted[0]),replaceAllQuot(splitted[1]),replaceAllQuot(district),
+                replaceAllQuot(splitted[2]),replaceAllQuot(splitted[6]),replaceAllQuot(splitted[10]));
     }
 }
