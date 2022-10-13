8faa7b198dac1c763e66c2fb1026e1d476a050ac
ohsuk kwon <percykwon@gmail.com>
2022-10-13 13:22:24 +0900

    [221013] 서울시 병의원 분석 1. parse 로직 수정

--- src/main/java/com/line/parser/HostpitalParser.java
+++ src/main/java/com/line/parser/HostpitalParser.java
@@ -4,18 +4,14 @@ import com.line.domain.Hospital;
 
 public class HostpitalParser implements Parser<Hospital> {
 
-
-    private String replaceAllQuot(String str) {
-        return str.replaceAll("\"", "");
-    }
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
 
         String[] splitted_address = splitted[1].split(" ");
         String district = splitted_address[0] + " " + splitted_address[1];
 
-        return new Hospital(replaceAllQuot(splitted[0]),replaceAllQuot(splitted[1]),replaceAllQuot(district),
-                replaceAllQuot(splitted[2]),replaceAllQuot(splitted[6]),replaceAllQuot(splitted[10]));
+        return new Hospital(splitted[0],splitted[1],district,splitted[2],splitted[6],splitted[10]);
     }
 }
