93c38bc5146ccd3b244a3406a8e035aabeb8bb09
ohsuk kwon <percykwon@gmail.com>
2022-10-19 13:43:45 +0900

    [221019] UPDATE : DB 연동 1. Spring 적용

--- src/main/java/com/dbexercise/dao/UserDaoFactory.java
+++ src/main/java/com/dbexercise/dao/UserDaoFactory.java
@@ -1,7 +1,12 @@
-package com.dbexercise;
+package com.dbexercise.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
 
+    @Bean
     public UserDao3 awsUserDao() {
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao3 userDao = new UserDao3(awsConnectionMaker);
--- src/test/java/com/dbexercise/UserDao3Test.java
+++ src/test/java/com/dbexercise/UserDao3Test.java
@@ -1,21 +1,33 @@
 package com.dbexercise;
 
+import com.dbexercise.dao.UserDao3;
+import com.dbexercise.dao.UserDaoFactory;
 import com.dbexercise.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDao3Test {
 
+    @Autowired
+    ApplicationContext context;
     @Test
     void addAndSelect() {
 
 //        UserDao3 userDao = new UserDao3(new AWSConnectionMaker());
 //        AWSUserDaoImpl userDao = new AWSUserDaoImpl();
-        UserDao3 userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("15", "EternityHwan", "1123");
+//        UserDao3 userDao = new UserDaoFactory().awsUserDao();
+        UserDao3 userDao = context.getBean("awsUserDao", UserDao3.class);
+        User user = new User("16", "EternityHwan", "1123");
         userDao.add(user);
 
-        User selectedUser = userDao.getById("15");
+        User selectedUser = userDao.getById("16");
         Assertions.assertEquals("EternityHwan",selectedUser.getName());
     }
 }
